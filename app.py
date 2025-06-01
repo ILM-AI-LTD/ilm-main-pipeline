57e18efed658bc103e8040e0fc544a2cff044c24
ffuad <mdtahmidhasanfuad@gmail.com>
2025-06-02 00:39:20 +0600

    CORS updated

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from scripts import generate_evaluation, generate_script
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app, origins="*")
 
 @app.route('/generate-script', methods=['POST'])
 def script_endpoint():
